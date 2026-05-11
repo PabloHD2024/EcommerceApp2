b3e4bb0fef741b491c6ab2174567618c9f7efd74
ignacio vidal <ignaciovidal@gmail.com>
2026-05-10 21:51:42 -0300

    Agrega CRUD de tickets

---
+++ src/data/ticketsData.js
@@ -0,0 +1,14 @@
+const ticketsDB = [
+    {
+        id_ticket: 1,
+        id_pedido: 1,
+        fecha_emision: "2026-05-10T21:30:00.000Z",
+        tipo_factura: "B",
+        total: 850000,
+        CAE: "12345678901234",
+        created_at: "2026-05-10T21:30:00.000Z",
+        updated_at: "2026-05-10T21:30:00.000Z"
+    }
+];
+
+module.exports = ticketsDB;
