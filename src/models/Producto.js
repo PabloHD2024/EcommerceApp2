d75b13eb55318fa8b4bfa5f7d008609be2e3c976
ignacio vidal <ignaciovidal@gmail.com>
2026-06-01 17:28:34 -0300

    Descuenta stock al finalizar checkout

--- src/models/Producto.js
+++ src/models/Producto.js
@@ -1,47 +1,47 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
 
-const Producto = sequelize.define('Producto', {
-    nombre: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    precio: {
-        type: DataTypes.FLOAT,
-        defaultValue: 0
-    },
-    stock: {
-        type: DataTypes.INTEGER,
-        defaultValue: 0
-    },
-    categoria: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },
-    image: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },
-    rating: {
-        type: DataTypes.FLOAT,
-        defaultValue: 0
-    },
-    reviews: {
-        type: DataTypes.INTEGER,
-        defaultValue: 0
-    },
+const Producto = sequelize.define("Producto", {
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  precio: {
+    type: DataTypes.FLOAT,
+    defaultValue: 0,
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  categoria: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  rating: {
+    type: DataTypes.FLOAT,
+    defaultValue: 0,
+  },
+  reviews: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
 
-    // Vigencia del producto
-    validFrom: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW
-    },
-    validTo: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: new Date('2099-12-31')
-    }
+  // Vigencia del producto
+  validFrom: {
+    type: DataTypes.DATE,
+    allowNull: true,
+    defaultValue: DataTypes.NOW,
+  },
+  validTo: {
+    type: DataTypes.DATE,
+    allowNull: true,
+    defaultValue: new Date("2099-12-31"),
+  },
 });
 
 module.exports = Producto;
