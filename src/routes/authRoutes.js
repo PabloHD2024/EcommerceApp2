4317925e7311841cc8fc62748f63c7a7aed456cd
Lucia Corral <lucorral8@gmail.com>
2026-06-07 11:30:06 -0300

    Limpiando archivos y logs temporales

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
-console.log("✅ authRoutes cargado");
-
 router.post("/login", authController.login);
 
 module.exports = router;
